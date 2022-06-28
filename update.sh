2609c2008de305e669b74bd33963f952e2923254
The AloneX Ofc <82893484+AlonezinX@users.noreply.github.com>
2022-06-28 13:04:49 -0300

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+CYAN="\033[1;36m"
+YELLOW="\033[1;33m"
+PURPLE="\033[1;35m"
+GREEN="\033[1;32m"
+RED='\033[1;31m'
+NC='\033[0m'
+
+echo $YELLOW"Você tem certeza que deseja atualizar a nezuko-md? [s/n]"
+read -p "> " opts
+
+if [ $opts = "s" ]; then
+
+clear
+echo $YELLOW"Atualizando a nezuko-md, aguarde..."
+git clone https://github.com/AlonezinX/nezuko-md2 > /dev/null 2> /dev/null
+
+cd nezuko-md2
+sh Venom.sh
+
+clear
+echo $GREEN"nezuko-md atualizado com sucesso!!"
+
+elif [ $opts = "n"]; then
+    clear
+    echo $RED"Atualização da nezuko-md abortada"
+    exit
+fi
